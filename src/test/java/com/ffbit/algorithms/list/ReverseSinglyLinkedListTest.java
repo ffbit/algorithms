e47aa3fea8f0f30e0343158a023f281cadc42159
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2015-02-01 00:35:59 +0200

    Refactored the ReverseSinglyLinkedListTest test suite.

--- src/test/java/com/ffbit/algorithms/list/ReverseSinglyLinkedListTest.java
+++ src/test/java/com/ffbit/algorithms/list/ReverseSinglyLinkedListTest.java
@@ -1,65 +1,41 @@
 package com.ffbit.algorithms.list;
 
 import com.ffbit.algorithms.list.ReverseSinglyLinkedList.ListNode;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
+@RunWith(JUnitParamsRunner.class)
 public class ReverseSinglyLinkedListTest {
     private ReverseSinglyLinkedList listReverser =
             new ReverseSinglyLinkedList();
 
     @Test
-    public void itShouldReverseLinkedListOfOneNode() {
-        ListNode<Integer> list = new ListNode<>(1);
-        ListNode<Integer> reversedList = new ListNode<>(1);
-
+    @Parameters(method = "data")
+    public void itShouldReverseLinkedListIteratively(ListNode<?> list,
+                                                     ListNode<?> reversedList) {
         assertThat(listReverser.reverse(list), is(reversedList));
     }
 
     @Test
-    public void itShouldReverseLinkedListOfTwoNodes() {
-        ListNode<Integer> list = new ListNode<>(1, new ListNode<>(2));
-        ListNode<Integer> reversedList = new ListNode<>(2, new ListNode<>(1));
-
-        assertThat(listReverser.reverse(list), is(reversedList));
-    }
-
-    @Test
-    public void itShouldReverseLinkedListOfTreeNodes() {
-        ListNode<Integer> list =
-                new ListNode<>(1, new ListNode<>(2, new ListNode<>(3)));
-        ListNode<Integer> reversedList =
-                new ListNode<>(3, new ListNode<>(2, new ListNode<>(1)));
-
-        assertThat(listReverser.reverse(list), is(reversedList));
-    }
-
-    @Test
-    public void itShouldReverseLinkedListOfOneNodeRecursively() {
-        ListNode<Integer> list = new ListNode<>(1);
-        ListNode<Integer> reversedList = new ListNode<>(1);
-
+    @Parameters(method = "data")
+    public void itShouldReverseLinkedListRecursively(ListNode<?> list,
+                                                     ListNode<?> reversedList) {
         assertThat(listReverser.reverseRecursively(list), is(reversedList));
     }
 
-    @Test
-    public void itShouldReverseLinkedListOfTwoNodesRecursively() {
-        ListNode<Integer> list = new ListNode<>(1, new ListNode<>(2));
-        ListNode<Integer> reversedList = new ListNode<>(2, new ListNode<>(1));
-
-        assertThat(listReverser.reverseRecursively(list), is(reversedList));
-    }
-
-    @Test
-    public void itShouldReverseLinkedListOfTreeNodesRecursively() {
-        ListNode<Integer> list =
-                new ListNode<>(1, new ListNode<>(2, new ListNode<>(3)));
-        ListNode<Integer> reversedList =
-                new ListNode<>(3, new ListNode<>(2, new ListNode<>(1)));
-
-        assertThat(listReverser.reverseRecursively(list), is(reversedList));
+    Object[][] data() {
+        return new Object[][]{
+                {new ListNode<>(1), new ListNode<>(1)},
+                {new ListNode<>(1, new ListNode<>(2)),
+                        new ListNode<>(2, new ListNode<>(1))},
+                {new ListNode<>(1, new ListNode<>(2, new ListNode<>(3))),
+                        new ListNode<>(3, new ListNode<>(2, new ListNode<>(1)))},
+        };
     }
 
 }
